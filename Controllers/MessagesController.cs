fc6a9734279b58605829c620638e18f5fe60a202
copokbl <zaneharrison2006@icloud.com>
2022-06-08 17:02:55 +1000

    Added gitignore

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -8,9 +8,11 @@ namespace BasicChatApi.Controllers;
 public class MessagesController : ApiController {
     
     [HttpGet]
-    public IEnumerable<Message> Get([FromQuery] int limit = 10, [FromQuery] int offset = 0) => Program.Storage.GetMessages(limit, offset);
+    public IEnumerable<Message> Get([FromQuery] int limit = 10, [FromQuery] int offset = 0) 
+        => Program.Storage.GetMessages(limit, offset);
     
     [HttpPost]
-    public void Post([FromBody] IncomingMessage message) => Program.Storage.CreateMessage(message.ToMessage());
+    public void Post([FromBody] IncomingMessage message) 
+        => Program.Storage.CreateMessage(message.ToMessage());
     
 }
